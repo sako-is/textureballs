51de3cf39291a484c93d5e38d3448d0688d3dc52
sako-is <isazadasaid@gmail.com>
2022-01-27 17:26:55 -0500

    you can move :)

--- game.py
+++ game.py
@@ -1,16 +1,21 @@
 from pyexpat import model
+from string import whitespace
 import sys
 from turtle import position
 from ursina import *
 from ursina.shaders import lit_with_shadows_shader
+from ursina.prefabs.first_person_controller import FirstPersonController
 
 Entity.default_shader = lit_with_shadows_shader
 app = Ursina()
 
-exit = Button(text='hello world!', color=color.red, scale=.1, text_origin=(0,0), position=(20, 200, 0))
-exit.on_click = application.quit
+# exit = Button(text='quit', color=color.red, scale=.1, text_origin=(0,0), position=(20, 200, 0))
+# exit.on_click = application.quit
+
 
 enableFB = False
+# enableFButton = Button(text='enable', color=color.white, scale = 0.1)
+
 sphereTexture = FileBrowser(file_types=('.*'), enabled=enableFB)
 userModel = "sphere"
 
@@ -22,11 +27,22 @@ ground = Entity(
     position = (0, -1, 0),
 )
 
-defaultCube = Entity(
-    model = userModel,
-    collider = "sphere",
-    texture = sphereTexture
-)
+class UserObject(Button):
+    def __init__(self, position = (0, 0, 0)):
+        super().__init__(
+            parent = scene,
+            model = userModel,
+            collider = "sphere",
+            texture = "brick", 
+            position = position,
+            color = color.white,
+            opacity = 1
+        )
+
+    def input(self, key):
+        if self.hovered:
+            if key == 'left mouse down':
+                self.y += 1
 
 
 window.color=color.light_gray.tint(.1)
@@ -37,7 +53,18 @@ sun = DirectionalLight()
 
 sun.look_at(Vec3(1,-1,-1))
 Sky()
+defaultObj = UserObject()
 
+player = FirstPersonController()
 EditorCamera()
+
+def update():
+    if held_keys['W']: player.x += 1
+    if held_keys['A']: player.y -= 1
+    if held_keys['S']: player.x -= 1
+    if held_keys['D']: player.y += 1
+    if held_keys['Space']: player.z += 1
+    if held_keys['E']: player.z += 1
+    if held_keys['Q']: player.z -= 1
 
 app.run()
